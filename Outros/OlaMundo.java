7ca1aab582231393ed5766cf03305103df2e0de8
Wilson de Farias <wilsonsf1@gmail.com>
2015-08-24 11:21:54 -0300

    2.14-2,3 Exercícios de impressão de linha

--- Outros/OlaMundo.java
+++ Outros/OlaMundo.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Teste de frase");
+        System.out.println("Segunda frase de teste");
+        
+        System.out.println("Fases separadas \npor comando de escape");
     }
 }
