af7ec31cba534046f60ea8f33f30f5d6431bba3f
Wilson de Farias <wilsonsf1@gmail.com>
2015-08-24 12:38:38 -0300

    3.13-1

---
+++ Cap3/Exercicio1.java
@@ -0,0 +1,9 @@
+class Exercicio1 {
+    public static void main (String [] args) {
+        int soma = 0;
+        for (int i = 150; i <= 300; i++) {
+            soma += i;
+        }
+        System.out.println("Soma dos números de 150 a 300: " + soma);
+    }
+}
