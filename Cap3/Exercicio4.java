c61c05a672440565f94967d079bf83575478c769
Wilson de Farias <wilsonsf1@gmail.com>
2015-08-24 13:06:19 -0300

    3.13-4 Fatorial de 1 a 10

---
+++ Cap3/Exercicio4.java
@@ -0,0 +1,9 @@
+class Exercicio4 {
+    public static void main (String [] args) {
+        int fatorial = 1;
+        for (int n = 1; n <= 10; n++) {
+            fatorial *= n;
+            System.out.println("Fatorial de ("+n+") : " + fatorial);
+        }
+    }
+}
