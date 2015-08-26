d826ca626b3b8ab688149967323b325c2c824448
Wilson de Farias <wilsonsf1@gmail.com>
2015-08-26 17:08:08 -0300

    8 treino no Eclipse

---
+++ fj11/src/Conta.java
@@ -0,0 +1,37 @@
+public class Conta {
+	private int numero;
+	private int agencia;
+	private double saldo;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		return false;
+	}
+}
