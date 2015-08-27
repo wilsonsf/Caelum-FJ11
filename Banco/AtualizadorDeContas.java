786887542c0e08891e190d462ae969a409aa051c
Wilson de Farias <wilsonsf1@gmail.com>
2015-08-27 09:19:29 -0300

    7.7-7 Criado teste da classe AtualizadorDeContas

--- Banco/AtualizadorDeContas.java
+++ Banco/AtualizadorDeContas.java
@@ -7,9 +7,9 @@ public class AtualizadorDeContas {
     }
     
     public void roda(Conta c) {
-        System.out.println(c.getSaldo());
+        System.out.println("Saldo anterior: " + c.getSaldo());
         c.atualiza(selic);
-        System.out.println(c.getSaldo());
+        System.out.println("Salto final: " + c.getSaldo());
         this.saldoTotal += c.getSaldo();
     }
     
@@ -24,6 +24,20 @@ public class AtualizadorDeContas {
 
 class TestaAtualizadorDeContas {
     public static void main (String[] args) {
-        Conta c = new Conta();       
+        Conta c = new Conta();
+        Conta cc = new ContaCorrente();
+        Conta cp = new ContaPoupanca();
+        
+        c.deposita(1000);
+        cc.deposita(1000);
+        cp.deposita(1000);
+        
+        AtualizadorDeContas adc = new AtualizadorDeContas(0.01);
+        
+        adc.roda(c);
+        adc.roda(cc);
+        adc.roda(cp);
+        
+        System.out.println("Saldo Total: " + adc.getSaldoTotal());
     }
 }
