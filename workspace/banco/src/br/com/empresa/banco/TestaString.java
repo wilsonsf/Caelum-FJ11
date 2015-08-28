9a470f32bae964c559f58022e9002b8282bcee45
Wilson de Farias <wilsonsf1@gmail.com>
2015-08-28 12:16:26 -0300

    14.10-2 Testes adicionais de String

--- workspace/banco/src/br/com/empresa/banco/TestaString.java
+++ workspace/banco/src/br/com/empresa/banco/TestaString.java
@@ -7,5 +7,16 @@ public class TestaString {
 		String s1 = s.replaceAll("1", "2");
 		System.out.println(s);
 		System.out.println(s1);
+
+		String teste = "fj";
+		System.out.println((s.contains(teste)) ? "Contém" : "Não contém");
+
+		String espacoEmBranco = "Nome Completo         ";
+
+		System.out.println(espacoEmBranco + ".");
+		System.out.println(espacoEmBranco.trim() + ".");
+
+		System.out.println((s.isEmpty()) ? "Está vazio" : "Não está vazio");
+		System.out.println("Tamanho de (" + s + ") : " + s.length());
 	}
 }
