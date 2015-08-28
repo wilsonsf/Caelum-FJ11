2bb8016781434a48bc099598ecb5fb6b32bc79b5
Wilson de Farias <wilsonsf1@gmail.com>
2015-08-28 12:11:14 -0300

    14.10-1 Teste das Strings

---
+++ workspace/banco/src/br/com/empresa/banco/TestaString.java
@@ -0,0 +1,11 @@
+package br.com.empresa.banco;
+
+public class TestaString {
+
+	public static void main(String[] args) {
+		String s = "fj11";
+		String s1 = s.replaceAll("1", "2");
+		System.out.println(s);
+		System.out.println(s1);
+	}
+}
