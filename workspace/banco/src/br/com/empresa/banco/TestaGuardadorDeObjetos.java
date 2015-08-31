afd25bc84eb87a264b6ff0697c8868eae211774a
Wilson de Farias <wilsonsf@1@gmail.com>
2015-08-30 21:08:22 -0300

    Finalização dos exerícios do capítulo 14.

--- workspace/banco/src/br/com/empresa/banco/TestaGuardadorDeObjetos.java
+++ workspace/banco/src/br/com/empresa/banco/TestaGuardadorDeObjetos.java
@@ -1,18 +1,18 @@
 package br.com.empresa.banco;
 
+import br.com.empresa.banco.conta.Conta;
 import br.com.empresa.banco.conta.ContaCorrente;
-import br.com.empresa.banco.conta.ContaPoupanca;
 import br.com.empresa.banco.conta.GuardadorDeObjetos;
 
 public class TestaGuardadorDeObjetos {
 
 	public static void main(String[] args) {
 		GuardadorDeObjetos guardador = new GuardadorDeObjetos();
-		ContaCorrente cc = new ContaCorrente(0);
+		Conta cc = new ContaCorrente(0);
 		guardador.adicionaObjeto(cc);
 		
-		ContaPoupanca cp = (ContaPoupanca) guardador.pegaObjeto(0);
+		Conta cp = (ContaCorrente) guardador.pegaObjeto(new Integer(0));
 
+		System.out.println(cp);
 	}
-
 }
