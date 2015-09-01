a0124da91c8242eb1cb06d5405d3db05e2ff980f
Wilson de Farias <wilsonsf@1@gmail.com>
2015-08-31 22:19:10 -0300

    15.8-8 Aplicação rodando como editor textos

--- teste-io/src/EntradaDeUMarquivo.java
+++ teste-io/src/EntradaDeUMarquivo.java
@@ -1,4 +1,3 @@
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
@@ -6,15 +5,12 @@ import java.util.Scanner;
 public class EntradaDeUMarquivo {
 
 	public static void main(String[] args) throws IOException {
-		Scanner entrada = new Scanner(
-//								System.in
-								new FileInputStream("arquivo.txt")
-								);
+		Scanner entrada = new Scanner(System.in);
 		
 		PrintStream ps = new PrintStream("saida.txt");
 		
-//		System.out.println("Digite sua mensagem:");
 		Integer contador = 0;
+		System.out.println("Digite sua mensagem: ");
 		while(entrada.hasNextLine()){
 			String linha = ++contador + " => " + entrada.nextLine();
 			ps.println(linha);
