24f3ea619a738d5f59c8b049d69d93d3eb266aa4
Wilson de Farias <wilsonsf@1@gmail.com>
2015-08-31 21:54:23 -0300

    15.8-2,3,4 Leitura de arquivo

--- teste-io/src/TestaEntrada.java
+++ teste-io/src/TestaEntrada.java
@@ -1,21 +1,24 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class TestaEntrada {
-
 	public static void main(String[] args) throws IOException {
-		InputStream is = System.in;
+//		InputStream is = System.in; // Leitura do teclado do sistema
+		InputStream is = new FileInputStream("arquivo.txt");
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
-		
+
 		System.out.println("Digite sua mensagem:");
 		String linha = br.readLine();
-		
+
 		while (linha != null) {
 			System.out.println(linha);
 			linha = br.readLine();
 		}
+		
+		br.close();
 	}
 }
