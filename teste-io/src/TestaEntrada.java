ba045d61b19df8b65c0b309bed74573306b03bab
Wilson de Farias <wilsonsf@1@gmail.com>
2015-08-31 21:56:56 -0300

    15.8-5 refatorando código de leitura de arquivo

--- teste-io/src/TestaEntrada.java
+++ teste-io/src/TestaEntrada.java
@@ -1,15 +1,19 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class TestaEntrada {
 	public static void main(String[] args) throws IOException {
 //		InputStream is = System.in; // Leitura do teclado do sistema
-		InputStream is = new FileInputStream("arquivo.txt");
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);
+//		InputStream is = new FileInputStream("arquivo.txt");
+//		InputStreamReader isr = new InputStreamReader(is);
+//		BufferedReader br = new BufferedReader(isr);
+		
+		BufferedReader br = new BufferedReader(
+								new InputStreamReader(
+									new FileInputStream("arquivo.txt")));
+		
 
 		System.out.println("Digite sua mensagem:");
 		String linha = br.readLine();
