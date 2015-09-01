84dd99198f5b6e28c9484a9a3c3fce409d1d1cdf
Wilson de Farias <wilsonsf@1@gmail.com>
2015-08-31 22:17:08 -0300

    15.8-7 Saida em arquivo usando PrintStream... Muito mais eficiente

--- teste-io/src/EntradaDeUMarquivo.java
+++ teste-io/src/EntradaDeUMarquivo.java
@@ -1,33 +1,25 @@
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class EntradaDeUMarquivo {
 
 	public static void main(String[] args) throws IOException {
-		InputStream is = new FileInputStream("arquivo.txt");
 		Scanner entrada = new Scanner(
 //								System.in
-								is
+								new FileInputStream("arquivo.txt")
 								);
 		
-		OutputStream os = new FileOutputStream("saida.txt");
-		OutputStreamWriter osw = new OutputStreamWriter(os);
-		BufferedWriter bw = new BufferedWriter(osw);
+		PrintStream ps = new PrintStream("saida.txt");
 		
-		System.out.println("Digite sua mensagem:");
+//		System.out.println("Digite sua mensagem:");
 		Integer contador = 0;
 		while(entrada.hasNextLine()){
-			String linha = ++contador + " : " + entrada.nextLine();
-			bw.write(linha);
-			bw.newLine();
+			String linha = ++contador + " => " + entrada.nextLine();
+			ps.println(linha);
 		}
 		entrada.close();
-		bw.close();
+		ps.close();
 	}
 }
