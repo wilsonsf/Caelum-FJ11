eb2a2df99db16ddb53beb7f4c745dbcc9aa523bd
Wilson de Farias <wilsonsf@1@gmail.com>
2015-08-31 22:12:59 -0300

    15.8-7 Testando saída para arquivo usando BufferedWriter

--- teste-io/src/EntradaDeUMarquivo.java
+++ teste-io/src/EntradaDeUMarquivo.java
@@ -1,19 +1,33 @@
-import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class EntradaDeUMarquivo {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		InputStream is = 
-				System.in;
-//				new FileInputStream("arquivo.txt");
-		Scanner entrada = new Scanner(is);
+	public static void main(String[] args) throws IOException {
+		InputStream is = new FileInputStream("arquivo.txt");
+		Scanner entrada = new Scanner(
+//								System.in
+								is
+								);
+		
+		OutputStream os = new FileOutputStream("saida.txt");
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(osw);
 		
 		System.out.println("Digite sua mensagem:");
+		Integer contador = 0;
 		while(entrada.hasNextLine()){
-			System.out.println(entrada.nextLine());
+			String linha = ++contador + " : " + entrada.nextLine();
+			bw.write(linha);
+			bw.newLine();
 		}
 		entrada.close();
+		bw.close();
 	}
 }
