8c9588f808e8a6797b268bed2926b6ad66e866dc
Wilson de Farias <wilsonsf1@gmail.com>
2015-10-25 12:53:46 -0300

    16.15-1,2 Comparação de eficiëncia utilizando List e Set

---
+++ banco/src/br/com/empresa/banco/teste/TestaPerformance.java
@@ -0,0 +1,82 @@
+package br.com.empresa.banco.teste;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class TestaPerformance {
+
+	public static void main(String[] args) {
+		System.out.println("iniciando...");
+		
+		System.out.println("Teste de inserção e consulta de 30mil objetos.");
+		 /**
+		  * Teste de inserção e consulta de 30mil.
+		  * ArrayList: 888 - 932ms | Média: 903.2 ms
+		  * HashSet: 	 2 -  28ms | Média: 9.0 ms
+		  */
+		
+		int numeroDeTestes = 5;
+		
+		Collection<Integer> testeList = new ArrayList<>();
+		Collection<Integer> testeSet = new HashSet<>();
+			
+		
+		System.out.print("Teste List: ");
+		System.out.println(" | Média: " + rodaNumeroDeTestes(numeroDeTestes, testeList)
+							+ " ms");
+
+		System.out.print("Teste Set:  ");
+		System.out.println(" | Média: " + rodaNumeroDeTestes(numeroDeTestes, testeSet)
+							+ " ms");
+		
+	}
+	
+	private static double rodaNumeroDeTestes(int numero, Collection<Integer> colecao) {
+		
+		long min = Long.MAX_VALUE;
+		long max = Long.MIN_VALUE;
+	
+		double media = 0;
+		for (int cont = 0; cont < numero; cont++) {
+			long atual = testaEficienciaDaColecao(colecao);
+			if (atual > max) max = atual;
+			if (atual < min) min = atual;
+			media += atual;
+		}
+		System.out.printf("%3d ms - %3d ms", min, max);
+		return media/numero;
+	}
+
+	private static long testaEficienciaDaColecao(Collection<Integer> teste) {
+		
+		long inicio = System.currentTimeMillis();
+		
+		int total = 30000;
+		
+//		long mediaParcial = 0;
+		for (int i = 0; i < total; i++) {
+//			long inicioOperacao = System.currentTimeMillis();
+			teste.add(i);
+//			mediaParcial += System.currentTimeMillis() - inicioOperacao;
+		}
+//		System.out.println("Media insercao: " + mediaParcial);
+		
+//		mediaParcial = 0;
+		for (int i = 0; i < total; i++) {
+//			long inicioOperacao = System.currentTimeMillis();
+			teste.contains(i);
+//			mediaParcial += System.currentTimeMillis() - inicioOperacao;
+		}
+//		System.out.println("Media busca: " + mediaParcial);
+		
+		long fim = System.currentTimeMillis();
+		long tempo = fim - inicio;
+		
+//		System.out.println("Tempo gasto: " + tempo);
+		return tempo;
+	}
+}
+
+
+
