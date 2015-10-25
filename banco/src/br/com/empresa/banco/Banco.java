c39967db164893aea0b9a57375bde3c09021c10a
Wilson de Farias <wilsonsf1@gmail.com>
2015-10-25 13:41:43 -0300

    16.15-6 Adicionada Indexação e pesquisa por nome
    Um atributo Map<String,Conta> foi adicionada a classe Banco, porém, isso
    não funcionará caso tenha-se mais de uma conta com o mesmo nome.

--- banco/src/br/com/empresa/banco/Banco.java
+++ banco/src/br/com/empresa/banco/Banco.java
@@ -1,15 +1,19 @@
 package br.com.empresa.banco;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import br.com.empresa.banco.conta.Conta;
 
 public class Banco {
 	List<Conta> contas = new ArrayList<Conta>();
+	Map<String,Conta> indexadoPorNome = new HashMap<String, Conta>();
 	
 	void add(Conta conta) {
 		contas.add(conta);
+		indexadoPorNome.put(conta.getCliente(), conta);
 	}
 	
 	Conta getContaAt(int posicao) {
@@ -18,5 +22,9 @@ public class Banco {
 
 	int getQuantidadeDeContas() {
 		return contas.size();
+	}
+	
+	Conta buscaPorNome(String nome) {
+		return indexadoPorNome.get(nome);
 	}
 }
