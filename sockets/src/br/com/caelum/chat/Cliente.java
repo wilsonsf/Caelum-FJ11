89969b6add6482fa5b431cd26a680d25e11e1112
Wilson de Farias <wilsonsf1@gmail.com>
2015-10-29 11:53:59 -0300

    19.8-2 Implantação da classe Cliente.

---
+++ sockets/src/br/com/caelum/chat/Cliente.java
@@ -0,0 +1,25 @@
+package br.com.caelum.chat;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Cliente {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket cliente = new Socket("127.0.0.1",12345);
+		System.out.println("O cliente se conectou ao servidor!");
+		
+		Scanner teclado = new Scanner(System.in);
+		PrintStream saida = new PrintStream(cliente.getOutputStream());
+		
+		while (teclado.hasNextLine()) {
+			saida.println(teclado.nextLine());
+		}
+		
+		saida.close();
+		teclado.close();
+		cliente.close();
+	}
+}
--- sockets/src/br/com/caelum/chat/Servidor.java
+++ sockets/src/br/com/caelum/chat/Servidor.java
@@ -21,7 +21,5 @@ public class Servidor {
 		
 		entrada.close();
 		servidor.close();
-		
 	}
-
 }
